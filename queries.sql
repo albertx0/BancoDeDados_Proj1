d061232875639e9966faefa00f5f49c52d6b6ea2
Kawan Mark Silva <128873271+KawanMark@users.noreply.github.com>
2025-04-01 19:57:41 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,15 +1,83 @@
--- Query 1: Mostre todo o histórico escolar de um aluno que teve reprovação em uma disciplina
-SELECT h1.AlunoRA, h1.DisciplinaCodigo, h1.Semestre AS SemestreReprovado, h1.Nota AS NotaReprovado, h2.Semestre AS SemestreAprovado, h2.Nota AS NotaAprovado
-FROM HistoricoEscolar h1
-JOIN HistoricoEscolar h2 ON h1.AlunoRA = h2.AlunoRA AND h1.DisciplinaCodigo = h2.DisciplinaCodigo
-WHERE h1.Situacao = 'Reprovado' AND h2.Situacao = 'Aprovado' AND h1.Semestre < h2.Semestre;
+--1. Histórico com reprovação e aprovação na disciplina
 
--- Query 2: Mostre todos os TCCs orientados por um professor junto com os nomes dos alunos
-SELECT t.Titulo, a.Nome
-FROM TCC t
-JOIN AlunoTCC at ON t.ID = at.TCCID
-JOIN Aluno a ON at.AlunoRA = a.RA
-WHERE t.OrientadorID = 1;
+SELECT 
+    a."ra_aluno",
+    a."nome_aluno" || ' ' || a."sobrenome_aluno" AS aluno,
+    d."id_disciplina",
+    d."nome_disciplina",
+    h_reprov."semestre" AS semestre_reprovacao,
+    h_reprov."media" AS nota_reprovacao,
+    h_aprov."semestre" AS semestre_aprovacao,
+    h_aprov."media" AS nota_aprovacao
+FROM "Historico_escolar" h_reprov
+JOIN "Historico_escolar" h_aprov ON 
+    h_reprov."ra_aluno" = h_aprov."ra_aluno" AND 
+    h_reprov."id_disciplina" = h_aprov."id_disciplina"
+JOIN "Aluno" a ON h_reprov."ra_aluno" = a."ra_aluno"
+JOIN "Disciplina" d ON h_reprov."id_disciplina" = d."id_disciplina"
+WHERE 
+    h_reprov."situacao" = 'Reprovado' AND 
+    h_aprov."situacao" = 'Aprovado' AND
+    h_reprov."semestre" < h_aprov."semestre";
 
--- Query 3: sei la
+--2. TCCs orientados por professor com alunos
+SELECT 
+    p."nome_professor" || ' ' || p."sobrenome_professor" AS orientador,
+    t."nome_trabalho" AS tcc,
+    STRING_AGG(a."nome_aluno" || ' ' || a."sobrenome_aluno", ', ') AS alunos
+FROM "TCC" t
+JOIN "Professor" p ON t."ra_orientador" = p."ra_professor"
+JOIN "TCC_aluno" ta ON t."id_tcc" = ta."id_tcc"
+JOIN "Aluno" a ON ta."ra_aluno" = a."ra_aluno"
+GROUP BY p."nome_professor", p."sobrenome_professor", t."nome_trabalho";
 
+
+--3. Matriz curricular de 2 cursos (2 queries)
+
+--Query para Ciência da Computação:
+
+sql
+Copy
+SELECT 
+    d."id_disciplina",
+    d."nome_disciplina"
+FROM "Disciplina" d
+JOIN "Curso" c ON d."id_curso" = c."id_curso"
+WHERE c."nome" = 'Ciência da Computação';
+
+-- Query para Ciência de Dados:
+
+sql
+Copy
+SELECT 
+    d."id_disciplina",
+    d."nome_disciplina"
+FROM "Disciplina" d
+JOIN "Curso" c ON d."id_curso" = c."id_curso"
+WHERE c."nome" = 'Ciência de Dados';
+
+--4. Disciplinas cursadas por aluno com professores
+SELECT 
+    a."ra_aluno",
+    a."nome_aluno" || ' ' || a."sobrenome_aluno" AS aluno,
+    d."id_disciplina",
+    d."nome_disciplina",
+    p."nome_professor" || ' ' || p."sobrenome_professor" AS professor
+FROM "Historico_escolar" h
+JOIN "Aluno" a ON h."ra_aluno" = a."ra_aluno"
+JOIN "Disciplina" d ON h."id_disciplina" = d."id_disciplina"
+JOIN "Professor" p ON d."ra_professor" = p."ra_professor"
+WHERE a."ra_aluno" = '5845-42136';  -- Substitua pelo RA do aluno
+
+-- 5. Chefes e coordenadores com substituição de nulos
+SELECT 
+    p."nome_professor" || ' ' || p."sobrenome_professor" AS professor,
+    COALESCE(d."nome_departamento", 'nenhum') AS departamento,
+    COALESCE(c."nome", 'nenhum') AS curso_coordenado
+FROM "Professor" p
+LEFT JOIN "Chefe_Departamento" cd ON p."ra_professor" = cd."ra_professor"
+LEFT JOIN "Departamento" d ON cd."id_depart" = d."id_depart"
+LEFT JOIN "Coordenador_Curso" cc ON p."ra_professor" = cc."ra_professor"
+LEFT JOIN "Curso" c ON cc."id_curso" = c."id_curso";
+
+-- procurar 10 queries extras no exercicio
