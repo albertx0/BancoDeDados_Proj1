9a32fbda41daa23093ccadbcd36a25d2e4adb817
albertx0 <126724209+albertx0@users.noreply.github.com>
2025-04-15 19:21:55 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -79,30 +79,84 @@
 --JOIN "Curso" c ON a.id_curso = c.id_curso
 --WHERE c.nome_curso = 'Ciencia da Computacao';
 
+-- /////////////////////////////////////////////////////
+
+-- 7) Liste as discplinas que foram lecionadas pelos professores Carlos e Maria.
+
+--SELECT DISTINCT p.nome_professor AS nome, d.id_disciplina AS "id da materia", d.nome_disciplina AS materia
+--FROM "Disciplina" d
+--JOIN "Professor" p ON d.ra_professor = p.ra
+--WHERE p.nome_professor IN ('Carlos', 'Maria');
 
 -- /////////////////////////////////////////////////////
 
+-- 8) Lista os cursos ministrados pelo professora Isabelly
+
+--SELECT DISTINCT c.id_curso, c.nome_curso
+--FROM "Curso" c
+--JOIN "Aluno" a ON c.id_curso = a.id_curso
+--JOIN "Historico_escolar" h ON a.ra = h.ra_aluno
+--JOIN "Disciplina" d ON h.id_disciplina = d.id_disciplina
+--JOIN "Professor" p ON d.ra_professor = p.ra
+--WHERE p.nome_professor = 'Isabelly';
 
 -- /////////////////////////////////////////////////////
 
+-- 9) Lista os nomes dos estudantes que não cursaram engenharia eletrica ou adminsitracao
+
+--SELECT a.nome_aluno AS nome, a.ra, c.nome_curso AS curso
+--FROM "Aluno" a
+--JOIN "Curso" c ON a.id_curso = c.id_curso
+--WHERE c.nome_curso NOT IN ('Engenharia Eletrica', 'Administracao');
 
 -- /////////////////////////////////////////////////////
 
+-- 10) Listar quantos TCCs cada proferssor orienta em ordem decrescente
+
+--SELECT p."ra", p."nome_professor", COUNT(t."id_tcc") AS quantidade_tccs
+--FROM "Professor" p
+--LEFT JOIN "TCC" t ON p."ra" = t."ra_orientador"
+--GROUP BY p."ra", p."nome_professor"
+--ORDER BY quantidade_tccs DESC
 
 -- /////////////////////////////////////////////////////
 
+-- 11) Listar todos TCCs dos alunos de ciencia da computação
+--SELECT  a."nome_aluno", a."sobrenome_aluno", ta."id_tcc", t."nome_trabalho"
+--FROM "Aluno" a
+--INNER JOIN "TCC_aluno" ta ON a."ra" = ta."ra_aluno"
+--INNER JOIN "TCC" t ON ta."id_tcc" = t."id_tcc"
+--INNER JOIN "Curso" c ON a."id_curso" = c."id_curso"
+--WHERE c."nome_curso" = 'Ciencia da Computacao';
 
 -- /////////////////////////////////////////////////////
 
+-- 12) Listar Todos TCCs do ano de 2020
+
+--SELECT "id_tcc", "nome_trabalho", "data", "ra_orientador", "status"
+--FROM "TCC"
+--WHERE EXTRACT(YEAR FROM "data") = 2020;
 
 -- /////////////////////////////////////////////////////
 
+-- 13) Listar todas materias do curso de ADM
+
+--SELECT d."id_disciplina" as id, d."nome_disciplina" as "nome"
+--FROM "Disciplina" d
+--INNER JOIN "Departamento" dep ON d."id_departamento" = dep."id_departamento"
+--INNER JOIN "Curso" c ON dep."id_departamento" = c."id_curso"
+--WHERE c."nome_curso" = 'Administracao';
 
 -- /////////////////////////////////////////////////////
 
-
--- /////////////////////////////////////////////////////
-
+-- 14) Lista o nome dos estudantes  que são orientados por um professor de engharia mecanica
+--SELECT a."nome_aluno" as "nome", a."sobrenome_aluno" as "sobrenome", p."nome_professor" as "orientador"
+--FROM "Aluno" a
+--INNER JOIN "TCC_aluno" ta ON a."ra" = ta."ra_aluno"
+--INNER JOIN "TCC" tcc ON ta."id_tcc" = tcc."id_tcc"
+--INNER JOIN "Professor" p ON tcc."ra_orientador" = p."ra"
+--INNER JOIN "Departamento" d ON p."ra" = d."ra_chefe_departamento"
+--WHERE d."nome_departamento" = 'Engenharia Mecanica';
 
 -- /////////////////////////////////////////////////////
 
