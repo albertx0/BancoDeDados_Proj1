ddc496c90855081d9ab89afbec8103607b111cb5
albertx0 <126724209+albertx0@users.noreply.github.com>
2025-04-15 19:18:36 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,83 +1,110 @@
---1. Histórico com reprovação e aprovação na disciplina
+-- 1) Mostre todo o histórico escolar de um aluno que teve reprovação em uma disciplina, 
+--retornando inclusive a reprovação em um semestre e a aprovação no semestre seguinte
 
-SELECT 
-    a."ra_aluno",
-    a."nome_aluno" || ' ' || a."sobrenome_aluno" AS aluno,
-    d."id_disciplina",
-    d."nome_disciplina",
-    h_reprov."semestre" AS semestre_reprovacao,
-    h_reprov."media" AS nota_reprovacao,
-    h_aprov."semestre" AS semestre_aprovacao,
-    h_aprov."media" AS nota_aprovacao
-FROM "Historico_escolar" h_reprov
-JOIN "Historico_escolar" h_aprov ON 
-    h_reprov."ra_aluno" = h_aprov."ra_aluno" AND 
-    h_reprov."id_disciplina" = h_aprov."id_disciplina"
-JOIN "Aluno" a ON h_reprov."ra_aluno" = a."ra_aluno"
-JOIN "Disciplina" d ON h_reprov."id_disciplina" = d."id_disciplina"
-WHERE 
-    h_reprov."situacao" = 'Reprovado' AND 
-    h_aprov."situacao" = 'Aprovado' AND
-    h_reprov."semestre" < h_aprov."semestre";
+--SELECT 
+--    aluno.nome_aluno
+--    ,aluno.sobrenome_aluno
+--    ,disc.nome_disciplina
+--    ,disc.id_disciplina
+--    ,his.semestre 
+--    ,his.situacao
+--    ,his.media
+--FROM "Historico_escolar" his
+--JOIN "Aluno" aluno ON his.ra_aluno = aluno.ra
+--JOIN "Disciplina" disc ON his.id_disciplina = disc.id_disciplina
+--WHERE his.ra_aluno = '020053511' 
+--ORDER BY his.semestre, disc.id_disciplina, his.situacao DESC
 
---2. TCCs orientados por professor com alunos
-SELECT 
-    p."nome_professor" || ' ' || p."sobrenome_professor" AS orientador,
-    t."nome_trabalho" AS tcc,
-    STRING_AGG(a."nome_aluno" || ' ' || a."sobrenome_aluno", ', ') AS alunos
-FROM "TCC" t
-JOIN "Professor" p ON t."ra_orientador" = p."ra_professor"
-JOIN "TCC_aluno" ta ON t."id_tcc" = ta."id_tcc"
-JOIN "Aluno" a ON ta."ra_aluno" = a."ra_aluno"
-GROUP BY p."nome_professor", p."sobrenome_professor", t."nome_trabalho";
+-- /////////////////////////////////////////////////////
+
+-- 2) Mostre todos os TCCs orientados por um professor junto com os nomes dos alunos que fizeram o projeto
+
+--SELECT 
+--    tcc.nome_trabalho
+--    ,tcc.id_tcc
+--    ,aluno.nome_aluno
+--    ,aluno.sobrenome_aluno
+--    ,tcc.ra_orientador
+--FROM "TCC" AS tcc
+--JOIN "TCC_aluno" AS tccAluno ON tcc.id_tcc = tccAluno.id_tcc
+--JOIN "Aluno" AS aluno ON tccAluno.ra_aluno = aluno.ra
+--WHERE tcc.ra_orientador = '514041791'
+--ORDER BY tcc.id_tcc
+
+-- /////////////////////////////////////////////////////
+
+-- 3)
+
+-- /////////////////////////////////////////////////////
+
+-- 4) Para um determinado aluno, mostre os códigos e nomes das diciplinas já cursadas 
+--junto com os nomes dos professores que lecionaram a disciplina para o aluno
+
+--SELECT DISTINCT
+--    aluno.nome_aluno
+--    ,aluno.sobrenome_aluno
+--    ,aluno.ra
+--    ,disc.id_disciplina
+--    ,disc.nome_disciplina
+--    ,prof.nome_professor
+--    ,prof.sobrenome_professor
+--FROM "Aluno" as aluno
+--JOIN "Historico_escolar" AS his ON his.ra_aluno = aluno.ra
+--JOIN "Disciplina" AS disc ON disc.id_disciplina = his.id_disciplina
+--JOIN "Professor" AS prof ON disc.ra_professor = prof.ra
+--WHERE aluno.ra = '039636825'
+--ORDER BY disc.nome_disciplina
+
+-- /////////////////////////////////////////////////////
+
+-- 5) Liste todos os chefes de departamento e coordenadores de curso em apenas uma query de forma que a primeira coluna seja o nome do professor, 
+-- a segunda o nome do departamento coordena e a terceira o nome do curso que coordena. 
+-- Substitua os campos em branco do resultado da query pelo texto "nenhum"
+
+--SELECT 
+--    prof.nome_professor
+--    ,COALESCE(depart.nome_departamento, 'Nenhum') AS nome_departamento
+--    ,COALESCE(curso.nome_curso, 'Nenhum') AS nome_curso
+--FROM "Professor" AS prof
+--LEFT JOIN "Departamento" AS depart ON depart.ra_chefe_departamento = prof.ra
+--LEFT JOIN "Curso" AS curso ON curso.ra_cordenador_curso = prof.ra
+--ORDER BY prof.ra
+
+-- /////////////////////////////////////////////////////
+
+-- 6) Encontra os nomes de todos os estudantes que cursaram Cinecia da computação 
+
+--SELECT a.nome_aluno as nome, a.sobrenome_aluno as sobrenome
+--FROM "Aluno" a
+--JOIN "Curso" c ON a.id_curso = c.id_curso
+--WHERE c.nome_curso = 'Ciencia da Computacao';
 
 
---3. Matriz curricular de 2 cursos (2 queries)
+-- /////////////////////////////////////////////////////
 
---Query para Ciência da Computação:
 
-sql
-Copy
-SELECT 
-    d."id_disciplina",
-    d."nome_disciplina"
-FROM "Disciplina" d
-JOIN "Curso" c ON d."id_curso" = c."id_curso"
-WHERE c."nome" = 'Ciência da Computação';
+-- /////////////////////////////////////////////////////
 
--- Query para Ciência de Dados:
 
-sql
-Copy
-SELECT 
-    d."id_disciplina",
-    d."nome_disciplina"
-FROM "Disciplina" d
-JOIN "Curso" c ON d."id_curso" = c."id_curso"
-WHERE c."nome" = 'Ciência de Dados';
+-- /////////////////////////////////////////////////////
 
---4. Disciplinas cursadas por aluno com professores
-SELECT 
-    a."ra_aluno",
-    a."nome_aluno" || ' ' || a."sobrenome_aluno" AS aluno,
-    d."id_disciplina",
-    d."nome_disciplina",
-    p."nome_professor" || ' ' || p."sobrenome_professor" AS professor
-FROM "Historico_escolar" h
-JOIN "Aluno" a ON h."ra_aluno" = a."ra_aluno"
-JOIN "Disciplina" d ON h."id_disciplina" = d."id_disciplina"
-JOIN "Professor" p ON d."ra_professor" = p."ra_professor"
-WHERE a."ra_aluno" = '5845-42136';  -- Substitua pelo RA do aluno
 
--- 5. Chefes e coordenadores com substituição de nulos
-SELECT 
-    p."nome_professor" || ' ' || p."sobrenome_professor" AS professor,
-    COALESCE(d."nome_departamento", 'nenhum') AS departamento,
-    COALESCE(c."nome", 'nenhum') AS curso_coordenado
-FROM "Professor" p
-LEFT JOIN "Chefe_Departamento" cd ON p."ra_professor" = cd."ra_professor"
-LEFT JOIN "Departamento" d ON cd."id_depart" = d."id_depart"
-LEFT JOIN "Coordenador_Curso" cc ON p."ra_professor" = cc."ra_professor"
-LEFT JOIN "Curso" c ON cc."id_curso" = c."id_curso";
+-- /////////////////////////////////////////////////////
 
--- procurar 10 queries extras no exercicio
+
+-- /////////////////////////////////////////////////////
+
+
+-- /////////////////////////////////////////////////////
+
+
+-- /////////////////////////////////////////////////////
+
+
+-- /////////////////////////////////////////////////////
+
+
+-- /////////////////////////////////////////////////////
+
+
+-- /////////////////////////////////////////////////////
